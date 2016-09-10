c8a89812346792b74f841f3b6a554a700198e3bb
Christian Maverick S. Empalmado <christian_empalmado@yahoo.com>
2016-09-10 11:39:25 +0800

    Adds strcat

--- lab3.cpp
+++ lab3.cpp
@@ -11,7 +11,7 @@ char* myStrCmp(char* str1, char* str2){
   		cout<<"equal";
   		break;
   	case 1:
-  		cout<<"positive";
+		cout<<"positive";
   		break;
   }
 }
@@ -19,11 +19,18 @@ char* myStrCmp(char* str1, char* str2){
 void myStrCpy (char* str1, char* str2){
 	strcpy(str1, str2);
 }
+
+void myStrCat (char* str1, char* str2){
+	strcat(str1, str2);
+}
+
+
 int main(){
   char str1[100]="Hello";
   char str2[100]="hello";
   
   myStrCmp(str1, str2);
   myStrCpy(str1, str2);
+  myStrCat(str1, str2);
   cout<<str1;
 }
